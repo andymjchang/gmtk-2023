96d6b32717a0d1534e4e8c1782176fc6ad849138
andymjchang <amjchang88@gmail.com>
2023-07-08 13:00:38 -0500

    timed buttons

--- Objects/PlacementDetector.gd
+++ Objects/PlacementDetector.gd
@@ -8,3 +8,8 @@ func _on_Area2D_input_event(_viewport, _event, _shape_idx):
 	if Input.is_action_just_pressed("mouse_left") and tileIsFree:
 		emit_signal("pressed")
 		print("pressed " + str(detectorIndex))
+		$Sprite.visible = true
+		$Timer.start()
+
+func _on_Timer_timeout():
+	$Sprite.visible = false
