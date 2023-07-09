ebd1c80f48ae793f49b9dbdf85ef1ab01f0e19a3
andymjchang <amjchang88@gmail.com>
2023-07-09 10:14:40 -0500

    hover cat selection

--- Objects/UnitPortrait.gd
+++ Objects/UnitPortrait.gd
@@ -2,4 +2,6 @@ extends AnimatedSprite
 
 func load_info(p_animName, p_name, p_dmg):
 	get_node("PortraitLabel").text = p_name + "\nDMG:" + str(p_dmg)
-	animation = p_animName 
+	animation = p_animName
+	
+	 
